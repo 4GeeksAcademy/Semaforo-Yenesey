d8666f223ea5ce1d7aa1af660e345bcea9587c5c
Yenesey2001 <yeneseyclase@gmail.com>
2025-02-17 10:22:59 +0000

    Semaforo hecho, añadiendo un setinterval para que funcione como uno real

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Bootstrap
@@ -11,18 +11,49 @@ import '../styles/index.css';
 // Components
 import Semaforo from './components/Semaforo';
 
+
+// He añadido un SetInterval que cambia de color como un semaforo real
 function App() {
-  const [color, setColor] = useState(""); 
+  const [color, setColor] = useState("rojo");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setColor((prevColor) => {
+        if (prevColor === "rojo") return "verde";
+        if (prevColor === "verde") return "amarillo";
+        return "rojo";
+      });
+    }, color === "amarillo" ? 3000 : 10000);
+
+    return () => clearInterval(intervalId);
+  }, [color]);
 
   function elegirColor(nuevoColor) {
-    setColor(nuevoColor); 
+    setColor(nuevoColor);
+  }
+
+  function colorRnd() {
+    // color random
+    const colores = ["rojo", "amarillo", "verde"];
+    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    setColor(`${colorAleatorio}`);
+  }
+
+  function colorPurple() {
+    // Purpura
+    const colores = ["rojo", "amarillo", "verde"];
+    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    setColor(`${colorAleatorio}-purpura`);
   }
 
   return (
     <div>
-      <Semaforo color={color} elegirColor={elegirColor} /> {/* Pasamos 'color' y 'elegirColor' como props */}
+      <Semaforo color={color} elegirColor={elegirColor} />
+      <button onClick={colorPurple}>Purpura!</button>
+      <button onClick={colorRnd}>Random</button>
     </div>
   );
 }
 
+  // Render
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
